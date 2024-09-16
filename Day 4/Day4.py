81adf4e2bdf95cc0b2026a64a703fa11ea7f9900
Anindita Deb <153967732+dita-deb@users.noreply.github.com>
2024-09-15 23:59:06 -0400

    Update Day4.py

--- Day 4/Day4.py
+++ Day 4/Day4.py
@@ -1,8 +1,11 @@
 #Random Module
-import random 
+import random
+import my_module
 
 random_integer = random.randint(1, 10)
 print(random_integer)
+
+#print(my_module.my_favorite_number)   #this is how you'd call a module into the main file
 
 random_number_0_to_1 = random.random() * 10
 print(random_number_0_to_1)
