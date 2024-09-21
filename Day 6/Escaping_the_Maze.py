5f09f933b2f590a1d971d63c03888bd4613fa0b9
Anindita Deb <153967732+dita-deb@users.noreply.github.com>
2024-09-21 02:00:16 -0400

    Update Escaping_the_Maze.py

--- Day 6/Escaping_the_Maze.py
+++ Day 6/Escaping_the_Maze.py
@@ -2,8 +2,12 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
-    
-while not at_goal():
+
+while front_is_clear():
+    move()     # move till it finds a wall
+turn_left()    # turn so that the wall is on the right #additionally we leave that while loop
+
+while not at_goal():    #now we finally have a wall on the right to enter this while loop
     if right_is_clear():
         turn_right()
         move()
