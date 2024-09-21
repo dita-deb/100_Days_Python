19f5fc32cea4d3c57ff5d1b2bcb9d9cce8895f58
Anindita Deb <153967732+dita-deb@users.noreply.github.com>
2024-09-20 23:09:09 -0400

    Create Day6.py

---
+++ Day 6/Day6.py
@@ -0,0 +1,8 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()   #improves the overall readability of codewill print all parts of this function line by line
+
+#the next part of this course actually utilizes Reeborg's World to help practice functions
